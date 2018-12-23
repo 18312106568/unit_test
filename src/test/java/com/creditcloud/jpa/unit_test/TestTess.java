52b26eaccb56b61ccab105ed282fb6be04e84730
18312106568 <315077558@qq.com>
2018-12-24 00:31:10 +0800

    添加文字识别

---
+++ src/test/java/com/creditcloud/jpa/unit_test/TestTess.java
@@ -0,0 +1,42 @@
+package com.creditcloud.jpa.unit_test;
+
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+import org.junit.Test;
+
+import java.io.File;
+
+public class TestTess {
+
+    @Test
+    public void testPng(){
+        ITesseract instance = new Tesseract();
+        instance.setDatapath(System.getProperty("user.dir")+"//tess");
+        instance.setLanguage("eng");
+
+        try {
+            String result =  instance.doOCR(new File(System.getProperty("user.dir")+"/images/001.png"));
+            System.out.println(result);
+        } catch (TesseractException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void testJpg(){
+        ITesseract instance = new Tesseract();
+        instance.setDatapath(System.getProperty("user.dir")+"//tess");
+        instance.setLanguage("eng");
+
+        try {
+            String result =  instance.doOCR(new File(System.getProperty("user.dir")+"/images/002.jpg"));
+            System.out.println(result);
+        } catch (TesseractException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
