ea0eac6feff6daf62673e2753114b51e23192240
MRB <bairongming@gzmpc.com>
2019-03-06 08:42:56 +0800

    poi

--- src/test/java/com/creditcloud/jpa/unit_test/TestXssheet.java
+++ src/test/java/com/creditcloud/jpa/unit_test/TestXssheet.java
@@ -5,23 +5,15 @@
  */
 package com.creditcloud.jpa.unit_test;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import org.junit.Test;
-
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.poifs.nio.DataSource;
-import org.apache.poi.poifs.nio.FileBackedDataSource;
-import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
-//import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.junit.Test;
+
+import java.io.*;
+import java.util.Iterator;
 /**
  *
  * @author MRB
@@ -34,10 +26,10 @@ public class TestXssheet {
     @Test
     public void testSheet() throws IOException {
         Workbook wb = null;  
-        File file = new File("G://2199811.xls");
+        File file = new File("E://doc//需上传码上放心平台品规.xlsx");
         InputStream in = new FileInputStream(file);
         long start = System.currentTimeMillis();
-        wb = new HSSFWorkbook(in);  
+        wb = new XSSFWorkbook(in);
         long end = System.currentTimeMillis();
         System.out.println("speed time:"+(end-start));
         
@@ -47,14 +39,43 @@ public class TestXssheet {
         System.out.println(sheet.getLastRowNum());
         StringBuilder sb = new StringBuilder();
         for(Row row : sheet){
-           Iterator it = row.cellIterator();
-           while(it.hasNext()){
-               sb.append(it.next().toString());
-           }
+//           Iterator it = row.cellIterator();
+//           while(it.hasNext()){
+//               sb.append(it.next().toString());
+//           }
+            System.out.println(row.getCell(2).toString());//+row.getCell(2).getCellType()
            sb.append('\n');
         }
          end = System.currentTimeMillis();
          System.out.println("speed time:"+(end-start));
          System.out.println(sb.toString());
+    }
+
+    @Test
+    public void exportCompany() throws IOException {
+        Workbook wb = null;
+        File file = new File("E://doc//往来单位批量导入-广州医药.xls");
+        InputStream in = new FileInputStream(file);
+        OutputStream out = new FileOutputStream(file);
+
+        wb = new HSSFWorkbook(in);
+        Sheet sheet = wb.getSheetAt(0);
+        int index=0;
+        for(Row row : sheet) {
+            index++;
+            if (index <= 2) {
+                continue;
+            }
+            Iterator it = row.cellIterator();
+            while(it.hasNext()){
+                System.out.println(it.next().toString());
+            }
+        }
+        Row row = sheet.createRow(sheet.getLastRowNum()+1);
+        row.createCell(0).setCellValue("123");
+        row.createCell(1).setCellValue("123");
+        wb.write(out);
+        wb.close();
+        out.close();
     }
 }
--- src/test/java/com/creditcloud/jpa/unit_test/Thread/TestFuture.java
+++ src/test/java/com/creditcloud/jpa/unit_test/Thread/TestFuture.java
@@ -4,9 +4,13 @@
  * and open the template in the editor.
  */
 package com.creditcloud.jpa.unit_test.Thread;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.FutureTask;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.junit.Test;
 /**
  *
@@ -31,5 +35,43 @@ public class TestFuture {
         }
         num2=task.get();
         System.out.println(num1+num2);
+    }
+
+
+    @Test
+    public void testDoss() throws ExecutionException, InterruptedException {
+        long start = System.currentTimeMillis();
+        ExecutorService executorService = Executors.newFixedThreadPool(8);
+        List<FutureTask<String>> futureTasks = new ArrayList<>();
+        for(int i=0;i<500;i++){
+            Callable<String> call = new HttpCallable("http://www.baidu.com");
+            FutureTask task = new FutureTask(call);
+            futureTasks.add(task);
+            executorService.submit(task);
+        }
+
+        for(FutureTask<String> task : futureTasks){
+            System.out.println(task.get());
+        }
+        long end = System.currentTimeMillis();
+        System.out.println(String.format("the request cost %d",(end-start)));
+
+    }
+
+    static class HttpCallable implements  Callable<String>{
+
+        private String url;
+
+        public HttpCallable(String url){
+            this.url = url;
+        }
+
+        @Override
+        public String call() throws Exception {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder().url(url).build();
+            Response response = client.newCall(request).execute();
+            return response.body().string();
+        }
     }
 }
